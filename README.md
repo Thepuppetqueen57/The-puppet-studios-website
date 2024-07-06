b062891d8b8f9427876ee9ec3b8c1a62128b99f2
Puppet <122136792+Thepuppetqueen57@users.noreply.github.com>
2024-07-05 22:45:38 -0700

    Rewrote it in svelte
    No more html lollers

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Info
-This is the puppet studios website. Made with html, css cuz I tried sveltekit but it didnt work at all
-Nothing else to say really.
+This is the puppet studios website. Now made with svelte.
---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  function cbosbarbarians() {
+    window.location.href = 'https://thepuppet57.141412.xyz/tps/cbos';
+  }
+
+  function navigateToInfo() {
+    window.location.href = 'https://thepuppet57.141412.xyz/tps/info.html';
+  }
+</script>
+
+<main>
+  <div id="some-bad-id-cuz-idk-what-to-name-these">
+    <h1 class="bigboytext">The Puppet Studios!</h1>
+    <div class="button-container">
+      <button class="booton" on:click={navigateToInfo}>Info</button>
+      <button class="booton" on:click={cbosbarbarians}>C-Bos</button>
+    </div>
+  </div>
+</main>
+
+<style lang="scss">
+  $purple: #370664;
+  $purpletext: #420068;
+  $lightgray: #b6b6b6;
+
+  :global(body), :global(html) {
+    overflow: hidden;
+    height: 100%;
+    margin: 0;
+    background-color: black;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .bigboytext {
+    color: $purpletext;
+    margin-bottom: 5px;
+  }
+
+  .button-container {
+    display: flex;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .booton {
+    color: $lightgray;
+    background-color: $purple;
+    width: 90px;
+    height: 40px;
+    border: none;
+    border-radius: 10px;
+    transition: 0.3s;
+    cursor: pointer;
+  }
+
+  .booton:hover {
+    transform: scale(1.1);
+    background-color: darken($purple, 3);
+  }
+</style>
